b0542a62b7c26775aded4af4e9c35c175ed83063
Harmonic <buygameraidee@gmail.com>
2020-01-09 18:32:27 +0700

    Transaction

--- application/views/Transaction.php
+++ application/views/Transaction.php
@@ -6,33 +6,7 @@
                                             <h2 style="">เงินในบัญชีของคุณ: 200.00 บาท</h2>
     </div>
     <hr>
-        <div class="message" style="padding: 30px; border-bottom: 1px solid #adb5bd;">
-                                    <div class="message-Hader mb-1" style="display: -webkit-flex;">
-                                        <div class="avatar" style="margin-right: 15px; width:">
-                                            <i class="fa fa-circle-08">
-                                            <i class="fa fa-credit-card" aria-hidden="true"></i>
-                                            </i>
-                                        </div>
-                                        <div class="question-item__header-center" style="-webkit-flex: 1;
-                                                                                        -ms-flex: 1;
-                                                                                        /* flex: 1; */
-                                                                                        /* width: 50%; */
-                                                                                        /* overflow: hidden; */margin-right: 20px;">
-                                            <div class="question-item__author truncate">
-                                                <p style="margin-bottom: 0px; font-weight: 600; font-size: 18px; width: max-content;">ฝาก</p>
-                                            </div>
-                                            <div class="question-item__date">
-                                                <p style="font-size: 14px; width: max-content;"> วันที่ </p>
-                                            </div>
-                                    </div>
-                                    </div>
-                                    <div class="question-item_Body" style="word-wrap: break-word;   overflow-wrap: break-word;  overflow: hidden;">
-                                            
-                                    </div>
-                                    <div class="question-item_like" align="right" style="align:right;">
-                                            <p style="color: #00a81f;">+ 200.00 บาท</p>
-                                    </div>
-        </div>
+       
         <div class="message" style="padding: 30px; border-bottom: 1px solid #adb5bd;">
                             <div class="message-Hader mb-1" style="display: -webkit-flex;">
                                 <div class="avatar" style="margin-right: 15px; width:">
