0f52c315c45e884fab24b0c2588dd27f54db412f
Harmonic <buygameraidee@gmail.com>
2020-02-12 12:11:00 +0700

    ok

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -5,8 +5,20 @@ class test extends CI_Controller {
 
     public function Hi()
     {
-        $id = $this->input->post('login');
-        echo json_encode(['status' => 1, 'msg' => $id]);
+        $query = $this->db->get('Shop');
+
+        foreach($query->result_array() as $data)
+        {
+            $object = array(
+                'Username'  =>  $data['ID_Shop'],
+                'Password'  =>  $data['Password']
+            );
+            $this->db->where('ID_User', $data['Id_Users']);
+            $this->db->where('ID_Type', 4);
+            $this->db->update('Users', $object);
+        }
+
+        echo "success";
 
         
     }
