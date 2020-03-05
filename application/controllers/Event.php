741cee60978178f9275088b30489310012d34df7
Tapanapong37729 <47583440+Tapanapong37729@users.noreply.github.com>
2020-03-05 13:33:32 +0700

    Update Event.php

--- application/controllers/Event.php
+++ application/controllers/Event.php
@@ -215,7 +215,7 @@ class Event extends CI_Controller {
                             'AmountJoin' => $this->input->post('Difday'),
                             'Id_TypeJoin' => '1',
                             'Amount' => $this->input->post('Amount'),
-                            'Borrow' => $show['ID_Users'],
+                            'Borrow' => $show['ID_User'],
                           );
                         
         
