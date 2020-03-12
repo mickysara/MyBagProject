dd910901ae9095ed7d19d23ed4128a0edf5fe34c
Harmonic <buygameraidee@gmail.com>
2020-03-12 18:47:11 +0700

    ok

---
+++ application/controllers/api/UploadMoney_api.php
@@ -0,0 +1,67 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+/** @noinspection PhpIncludeInspection */
+require APPPATH . '/libraries/REST_Controller.php';
+
+/**
+ * This is an example of a few basic user interaction methods you could use
+ * all done with a hardcoded array
+ *
+ * @package         CodeIgniter
+ * @subpackage      Rest Server
+ * @category        Controller
+ * @author          Phil Sturgeon, Chris Kacerguis
+ * @license         MIT
+ * @link            https://github.com/chriskacerguis/codeigniter-restserver
+ */
+class UploadMoney_api extends \Restserver\Libraries\REST_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+       
+        
+	}
+	
+	public function index_post()
+	{
+        $Money = $this->input->post("Money");
+        $Date       = $this->input->post("Date");
+        $Time     = $this->input->post("Time");
+        $User   =   $this->input->post("User"); 
+
+        
+        
+        $config['upload_path'] = './img/DepositUser/';
+        $config['allowed_types'] = '*';
+        $config['max_size']  = '1000000000';
+        $config['max_width']  = '1000000000';
+        $config['max_height']  = '1000000000';
+        
+        $this->upload->initialize($config);
+        
+        if ( ! $this->upload->do_upload('Image')){
+            $error = array('error' => $this->upload->display_errors());
+        }
+        else{
+            $data = array('upload_data' => $this->upload->data());
+            
+        }
+
+
+                
+        $this->response(array(
+            'status'	=> 	'Success',
+            'Money'     =>  $Money,
+            'Date'      =>  $Date,
+            'Time'      =>  $Time,
+            'User'      =>  $User,
+        ));
+        
+        
+	}
+
+}
