386170c8e177e3b37d2fe082f55a90eb1d0d03db
Tapanapong37729 <47583440+Tapanapong37729@users.noreply.github.com>
2020-03-16 01:00:03 +0700

    e

--- application/views/End.php
+++ application/views/End.php
@@ -12,7 +12,7 @@ $idRepo = ($arraystate2[6]);
 
 $result = $this->db->query("SELECT * FROM Activities WHERE ID_Activities = $idRepo"); ?>
 
-<?php   foreach($result->result_array() as $data)
+	<?php   foreach($result->result_array() as $data)
                                                     { ?>
 
 	<div class="ct-example tab-content tab-example-result" style="margin: auto; margin-top: 62px; padding: 1.25rem;
@@ -21,16 +21,16 @@ $result = $this->db->query("SELECT * FROM Activities WHERE ID_Activities = $idRe
 
 		<div id="inputs-alternative-component" class="tab-pane tab-example-result fade active show" role="tabpanel"
 			aria-labelledby="inputs-alternative-component-tab">
-            <h1 style="font-size: 30px;">สรุปผลกิจกรรม</h1>
-            <hr>
-            <h2 style="font-size: 20px; font-weight:bold">ชื่อกิจกรรม : <?php echo $data['Name_Activities'];?> </h2>
-               <?php
+			<h1 style="font-size: 30px;">สรุปผลกิจกรรม</h1>
+			<hr>
+			<h2 style="font-size: 20px; font-weight:bold">ชื่อกิจกรรม : <?php echo $data['Name_Activities'];?> </h2>
+			<?php
                $this->db->where('Id_TypeActivity', $data['Type']);
                 $querytype = $this->db->get('TypeActivities');
                 $showtype = $querytype->row_array();
                ?>
-						<p style="font-weight: 500;">ประเภทกิจกรรม : <?php echo $showtype['Name_TypeActivity'];?></p>
-						<p style="font-weight: 500;">วันที่จัดกิจกรรม : <?php 
+			<p style="font-weight: 500;">ประเภทกิจกรรม : <?php echo $showtype['Name_TypeActivity'];?></p>
+			<p style="font-weight: 500;">วันที่จัดกิจกรรม : <?php 
                                                                                             $var_date = $data['DateStart'];
                                                                                             $strDate = $var_date;
                                                                                             $strYear = date("Y",strtotime($strDate))+543;
@@ -44,7 +44,7 @@ $result = $this->db->query("SELECT * FROM Activities WHERE ID_Activities = $idRe
                                                 
                                                                                             echo $strDay." ".$strMonthThai." ".$strYear." เวลา ".$strH.":".$stri;
                                                                                         ?></p>
-						<p style="font-weight: 500;">วันที่สิ้นสุดกิจกรรม : <?php 
+			<p style="font-weight: 500;">วันที่สิ้นสุดกิจกรรม : <?php 
                                                                                             $var_date = $data['DateEnd'];
                                                                                             $strDate = $var_date;
                                                                                             $strYear = date("Y",strtotime($strDate))+543;
@@ -61,19 +61,19 @@ $result = $this->db->query("SELECT * FROM Activities WHERE ID_Activities = $idRe
 
 
 
-                        <p class="description">จำนวนเข้าร่วมขั้นต่ำ: <?php echo $data['AmountJoin'];?> วัน</p>
-                        <?php 
+			<p class="description">จำนวนเข้าร่วมขั้นต่ำ: <?php echo $data['AmountJoin'];?> วัน</p>
+			<?php 
                             $this->db->where('Id_location', $data['Id_location']);
                             $querylo = $this->db->get('Eventlocation');
                             $showlo = $querylo->row_array();
                         ?>
-                        <p class="description">สถานที่จัดกิจกรรม: <?php echo $showlo['NameLocation'];?></p>
-						<p class="description">รายละเอียด: <?php echo $data['Detail'];?></p>
+			<p class="description">สถานที่จัดกิจกรรม: <?php echo $showlo['NameLocation'];?></p>
+			<p class="description">รายละเอียด: <?php echo $data['Detail'];?></p>
 
 		</div>
 	</div>
 
-<!-------------------------------------------------- ข้อมูลกิจกรรม ---------------------------------------------------------->
+	<!-------------------------------------------------- ข้อมูลกิจกรรม ---------------------------------------------------------->
 
 	<div class="ct-example tab-content tab-example-result" style="margin: auto; margin-top: 32px; padding: 1.25rem;
                         border-radius: .25rem;
@@ -82,8 +82,17 @@ $result = $this->db->query("SELECT * FROM Activities WHERE ID_Activities = $idRe
 		<div id="inputs-alternative-component" class="tab-pane tab-example-result fade active show" role="tabpanel"
 			aria-labelledby="inputs-alternative-component-tab">
 			<h2 class="" style="font-size: 30px;">สรุปผู้เข้าร่วมกิจกรรม</h2>
-            <hr>
-            
+			<hr>
+			<?php   
+					$this->db->where('ID_Activities', $idRepo);
+					$this->db->group_by('ID_List');
+					$getid = $this->db->get('NameList');
+					
+					?>
+
+
+
+
 			<div class="table-responsive">
 				<table class="table align-items-center table-flush" id="Filesearch">
 					<thead class="thead-light">
@@ -103,7 +112,43 @@ $result = $this->db->query("SELECT * FROM Activities WHERE ID_Activities = $idRe
 						</tr>
 					</thead>
 					<tbody>
+						<?php  
+							$query4 = $this->db->query("SELECT student.Branch 
+							FROM NameList,student 
+							WHERE NameList.ID_List = student.Id_Users
+							AND NameList.ID_Activities = $idRepo
+							GROUP BY NameList.ID_List");
 
+					        $query = $this->db->query("SELECT student.Branch 
+							 FROM NameList,student 
+							 WHERE NameList.ID_List = student.Id_Users
+							 AND NameList.ID_Activities = $idRepo 
+							 GROUP BY Branch");
+							 
+							foreach ($query->result_array() as $Show)
+							{
+							$this->db->where('ID_Branch',$Show['Branch']);
+							$showbranch = $this->db->get('Branch');
+							$showbranch2 = $showbranch->row_array();
+							
+                            $eiei = $Show['Branch'];
+
+							$result = $this->db->query("SELECT student.Id_Users 
+							FROM NameList,student 
+							WHERE NameList.ID_List = student.Id_Users
+							AND NameList.ID_Activities = $idRepo
+							AND student.Branch = $eiei
+							GROUP BY NameList.ID_List");
+                             
+							$result2 = $this->db->query("SELECT NameList.ID_NameList 
+							FROM NameList,student 
+							WHERE NameList.ID_List = student.Id_Users
+							AND NameList.ID_Activities = $idRepo
+							AND student.Branch = $eiei
+							AND NameList.TimeIn IS NOT NULL
+							GROUP BY NameList.ID_List");
+							
+														  ?>
 
 						<tr>
 							<th scope="row">
@@ -113,7 +158,7 @@ $result = $this->db->query("SELECT * FROM Activities WHERE ID_Activities = $idRe
 									</a>
 									<div class="media-body">
 										<span class="mb-0 text-sm">
-											<p style="margin-bottom: 0px;"><?php echo $data['Name_Activities'];?></p>
+											<p style="margin-bottom: 0px;"><?php echo $showbranch2['Name_Branch'] ?></p>
 										</span>
 									</div>
 								</div>
@@ -121,16 +166,42 @@ $result = $this->db->query("SELECT * FROM Activities WHERE ID_Activities = $idRe
 			</th>
 			<td>
 				<p>
-					<?php echo $data['Type'];?>
+					<?php echo $result->num_rows()?>
 				</p>
 			</td>
 			<td>
-
-			</tr>
-			<?php }
+				<p>
+					<?php echo $result2->num_rows()?>
+				</p>
+			</td>
+			<?php $data1 = $result->num_rows();
+				  $data2 = $result2->num_rows();
+				  $cal = $data2 / $data1;
+				  $cal2 = $cal * 100;?>
+			<td>
+				<p>
+					<?php echo $cal2.('%')?>
+				</p>
+			</td>
+			<td>
+				</tr>
+				
+				<?php 
+				} 
+				$this->db->where('ID_Activities',$idRepo);
+				$this->db->group_by('ID_List');
+				$namelist = $this->db->get('NameList');
+							?>
+				<td>
+							<h2 style="text-align: center; font-weight:bold">ผู้เข้าร่วมทั้งหมด</h2>
+				</td>
+				<td>
+							<h2 style="font-weight:bold"><?php echo $query4->num_rows()?></h2>
+				</td>
+								<?php					}
                                                      ?>
-			</tbody>
-			</table>
+				</tbody>
+				</table>
 		</div>
 	</div>
 </div>
@@ -138,9 +209,26 @@ $result = $this->db->query("SELECT * FROM Activities WHERE ID_Activities = $idRe
 
 
 
+<!-- foreach($getid->result_array() as $showid)
+                                                { 
+							$this->db->where('ID_User', $showid['ID_List']);
+							$getus = $this->db->get('Users');
+							$showus = $getus->row_array();
+							
+							$this->db->where('ID_User', $showid['ID_List']);
+							$getus = $this->db->get('Users');
+							$showus = $getus->row_array();
 
+							$this->db->where('Id_student', $showus['Username']);
+							$getstu = $this->db->get('student');
+							
+							foreach($getstu->result_array() as $showstu)
+							{
+								$this->db->where('ID_Branch', $showstu['Branch']);
+								$getbranch = $this->db->get('Branch'); -->
 
-
+<!-- }
+} -->
 
 
 
@@ -176,113 +264,113 @@ $result = $this->db->query("SELECT * FROM Activities WHERE ID_Activities = $idRe
 						$showpayloan = (string)$calpayloan;
         ?>
 
-	<div class="ct-example tab-content tab-example-result" style="margin: auto; margin-top: 32px; margin-bottom: 32px; padding: 1.25rem;
+<div class="ct-example tab-content tab-example-result" style="margin: auto; margin-top: 32px; margin-bottom: 32px; padding: 1.25rem;
                 border-radius: .25rem;
                 background-color: #f7f8f9;">
 
-		<div id="inputs-alternative-component" class="tab-pane tab-example-result fade active show" role="tabpanel"
-			aria-labelledby="inputs-alternative-component-tab">
-            <h3 style="font-size: 30px;">ค่าใช้จ่ายภายในกิจกรรม</h3>
-            <hr>
-			<h3 style="font-size: 20px;"> งบประมาณกิจกรรม : <?php echo number_format($showshowbgstring, 2);?> บาท</h3>
+	<div id="inputs-alternative-component" class="tab-pane tab-example-result fade active show" role="tabpanel"
+		aria-labelledby="inputs-alternative-component-tab">
+		<h3 style="font-size: 30px;">ค่าใช้จ่ายภายในกิจกรรม</h3>
+		<hr>
+		<h3 style="font-size: 20px;"> งบประมาณกิจกรรม : <?php echo number_format($showshowbgstring, 2);?> บาท</h3>
 
-			
 
-			<div class="table-responsive">
-				<table class="table align-items-center table-flush" id="Filetable">
-					<thead class="thead-light">
-						<tr>
-							<th scope="col">
-								<h2 style="text-align: center; font-weight:bold">รายการ</h2>
-							</th>
-							<th style="text-align:center;" scope="col">
-								<h2 style="text-align: center; font-weight:bold">จำนวนเงิน (บาท)
-								</h2>
 
-							</th>
-							<th style="text-align:center;" scope="col">
-								<h2 style="text-align: center; font-weight:bold">จำนวนเงินที่ใช้</h2>
+		<div class="table-responsive">
+			<table class="table align-items-center table-flush" id="Filetable">
+				<thead class="thead-light">
+					<tr>
+						<th scope="col">
+							<h2 style="text-align: center; font-weight:bold">รายการ</h2>
+						</th>
+						<th style="text-align:center;" scope="col">
+							<h2 style="text-align: center; font-weight:bold">จำนวนเงิน (บาท)
+							</h2>
 
-							</th>
-							<th style="text-align:center;" scope="col">
-								<h2 style="text-align: center; font-weight:bold">คงเหลือ</h2>
+						</th>
+						<th style="text-align:center;" scope="col">
+							<h2 style="text-align: center; font-weight:bold">จำนวนเงินที่ใช้</h2>
 
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-						<?php                 	$this->db->where('ID_Activities', $idRepo);
+						</th>
+						<th style="text-align:center;" scope="col">
+							<h2 style="text-align: center; font-weight:bold">คงเหลือ</h2>
+
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php                 	$this->db->where('ID_Activities', $idRepo);
 												$query = $this->db->get('Loan');
 												
                                                 foreach($result->result_array() as $data)
                                                 {?>
-						<tr>
-							<th scope="row">
-								<span class="mb-0 text-sm">
-									<h2 style="margin-bottom: 0px; font-weight:bold ">
-										<?php   $this->db->where('Id_TypeLoan',$data['Type']);
+					<tr>
+						<th scope="row">
+							<span class="mb-0 text-sm">
+								<h2 style="margin-bottom: 0px; font-weight:bold ">
+									<?php   $this->db->where('Id_TypeLoan',$data['Type']);
 																		 $datashow = $this->db->get('TypeLoan');
 																		$showshow = $datashow->row_array()?>
-										<?php echo $showshow['Name_TypeLoan'];?></h2>
-								</span>
+									<?php echo $showshow['Name_TypeLoan'];?></h2>
+							</span>
 
-							</th>
-							<td>
+						</th>
+						<td>
 
-								<?php 	$type = $data['Type'];
+							<?php 	$type = $data['Type'];
 						 $moneyT = $this->db->query("SELECT SUM(Money) FROM Loan WHERE Type = '$type' and ID_Activities = $idRepo") ;
 						 $moneyType = $moneyT->row_array();?>
-								<h2 style="text-align: center; font-weight:bold">
-									<?php echo number_format($moneyType['SUM(Money)'], 2);
+							<h2 style="text-align: center; font-weight:bold">
+								<?php echo number_format($moneyType['SUM(Money)'], 2);
 															$i= $i+$moneyType['SUM(Money)'] ?></h2>
-							</td>
-							<td>
-								<h2 style="text-align: center; font-weight:bold"></h2>
-							</td>
-							<td>
-								<h2 style="text-align: center; font-weight:bold"></h2>
-							</td>
-							<td>
-								<h2 style="text-align: center; font-weight:bold"></h2>
-							</td>
-                            <td>
-								<h2 style="text-align: center; font-weight:bold"></h2>
-							</td>
-						</tr>
-						<?php foreach($query->result_array() as $datadetail)
+						</td>
+						<td>
+							<h2 style="text-align: center; font-weight:bold"></h2>
+						</td>
+						<td>
+							<h2 style="text-align: center; font-weight:bold"></h2>
+						</td>
+						<td>
+							<h2 style="text-align: center; font-weight:bold"></h2>
+						</td>
+						<td>
+							<h2 style="text-align: center; font-weight:bold"></h2>
+						</td>
+					</tr>
+					<?php foreach($query->result_array() as $datadetail)
 						{ 
 						if($data['Type'] == $datadetail['Type'])
 						{?>
-						<tr>
-							<th scope="row">
-								<span class="mb-0 text-sm">
-									<p style="margin-left: 35px;"> -
-										<?php echo $datadetail['Name_Loan'];?></p>
-								</span>
+					<tr>
+						<th scope="row">
+							<span class="mb-0 text-sm">
+								<p style="margin-left: 35px;"> -
+									<?php echo $datadetail['Name_Loan'];?></p>
+							</span>
 
-							</th>
-							<td>
-								<p style="text-align: center;">
-									<?php echo number_format($datadetail['Money'], 2) ?></p>
-							</td>
-							<td>
-								<p style="text-align: center;">
-									<?php echo number_format($datadetail['Money_Use'], 2) ?></p>
-							</td>
-							<td>
-								<p style="text-align: center;">
-									<?php echo number_format($datadetail['Sum'], 2) ?></p>
-							</td>
+						</th>
+						<td>
+							<p style="text-align: center;">
+								<?php echo number_format($datadetail['Money'], 2) ?></p>
+						</td>
+						<td>
+							<p style="text-align: center;">
+								<?php echo number_format($datadetail['Money_Use'], 2) ?></p>
+						</td>
+						<td>
+							<p style="text-align: center;">
+								<?php echo number_format($datadetail['Sum'], 2) ?></p>
+						</td>
 
-                                              
-						</tr>
-						<?php }
+
+					</tr>
+					<?php }
 				  } ?>
-						<?php } 
+					<?php } 
                                          ?>
-                                         
-						<tr>
-                        <?php $moneyget2 = $this->db->query("SELECT sum(Money_Use)
+
+					<tr>
+						<?php $moneyget2 = $this->db->query("SELECT sum(Money_Use)
                                     as moneyuse
                                     FROM Loan
                                     WHERE ID_Activities = '$idRepo'");
@@ -294,25 +382,27 @@ $result = $this->db->query("SELECT * FROM Activities WHERE ID_Activities = $idRe
                                     WHERE ID_Activities = '$idRepo'");
                         $sumget3 =  $moneyget3->row_array();?>
 
-                        
-							<td>
-								<h2 style="text-align: center; font-weight:bold">ยอดรวม</h2>
-							</td>
-							<td>
-								<h2 style="text-align: center; font-weight:bold"><?php echo number_format($i, 2);?></h2>
-							</td>
-                            <td>
-								<h2 style="text-align: center; font-weight:bold"><?php echo number_format($sumget2['moneyuse'], 2);?></h2>
-							</td>
-                            <td>
-								<h2 style="text-align: center; font-weight:bold"><?php echo number_format($sumget3['sumsum'], 2);?></h2>
-							</td>
-                            
-						</tr>
-					</tbody>
-				</table>
-			</div>
+
+						<td>
+							<h2 style="text-align: center; font-weight:bold">ยอดรวม</h2>
+						</td>
+						<td>
+							<h2 style="text-align: center; font-weight:bold"><?php echo number_format($i, 2);?></h2>
+						</td>
+						<td>
+							<h2 style="text-align: center; font-weight:bold">
+								<?php echo number_format($sumget2['moneyuse'], 2);?></h2>
+						</td>
+						<td>
+							<h2 style="text-align: center; font-weight:bold">
+								<?php echo number_format($sumget3['sumsum'], 2);?></h2>
+						</td>
+
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	</div>
+</div>
 </div>
 <!-------------------------------------------------- ข้อมูลกิจกรรม ---------------------------------------------------------->
